fd5c921b070ae17481a7fd2150ca18fd9f6b3ae1
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-19 23:21:57 -0300

    :feat: Suporte para execução do projeto via npm start

--- communication_backend/src/app.js
+++ communication_backend/src/app.js
@@ -1,14 +1,14 @@
 require('dotenv').config({ path: 'config.env' });
 
 const port = process.env.SERVER_PORT;
+const routesRecorder = require('./routes/routesRecorder');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+routesRecorder(app);
+
 if (port) {
-    const routesRecorder = require('./routes/routesRecorder');
-    const express = require('express');
-    const app = express();
-
-    app.use(express.json());
-    routesRecorder(app);
-
     if (require.main == module) {
         app.listen(port);
         console.log('Our endpoint is running...');
