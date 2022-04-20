ab42f7a9d926c285bc2aa5de873334d1778c58f0
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-20 00:03:16 -0300

    :feat: Adicionando a criação de um UUID no método que será responsável por implementar o agendamento do envio da comunicação

--- communication_backend/src/services/communicationRequestService.js
+++ communication_backend/src/services/communicationRequestService.js
@@ -1,5 +1,6 @@
+const {v4: uuidv4} = require('uuid');
 function registerCommunicationSend(req, res) {
-    const uniqueCode = 'MOCK_VALID_CODE';
+    const uniqueCode = uuidv4();
     res.status(200).json({ code: uniqueCode });
 }
 
