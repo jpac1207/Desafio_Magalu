0d0bfaf128eb1fb0b46613e2530b838043bafb93
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-21 12:38:50 -0300

    :test_tube: test: Adicionando testes para verificar os casos básicos de consulta de status de comunicação

--- communication_backend/src/app.test.js
+++ communication_backend/src/app.test.js
@@ -1,4 +1,5 @@
 const supertest = require('supertest');
+const { v4: uuidv4 } = require('uuid');
 const app = require('./app');
 
 function executeRegisterCommunicationTests() {
@@ -46,7 +47,7 @@ function executeRegisterCommunicationTests() {
         expect(response.body.error).toBeDefined();
     });
 
-    
+
     test('Refuse invalid delivery type', async () => {
         const response = await supertest(app).post('/communicationrequest/register').send({
             "deliveryTime": "2022-05-01T23:28:56.782Z",
@@ -59,4 +60,42 @@ function executeRegisterCommunicationTests() {
     });
 }
 
+function executeCheckCommunicationTest() {
+    test('Check if status of a new communication request is equals to waiting', async () => {
+        // Create communication request
+        let futureDate = new Date();
+        futureDate.setFullYear(futureDate.getFullYear() + 1);
+        const communicationRequestResponse = await supertest(app).post('/communicationrequest/register').send({
+            "deliveryTime": futureDate.toISOString(),
+            "receiverEmail": "email.teste@hotmail.com",
+            "message": "No minimo 10 caracteres",
+            "deliveryType": 1
+        });
+        let confirmationToken = communicationRequestResponse.body.communicationRequestCode;
+        // Check Status
+        const response = await supertest(app).post('/communicationrequest/check').send({
+            "communicationRequestToken": confirmationToken
+        });
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.communicationRequest).toBeDefined();
+        expect(response.body.communicationRequest.status).toEqual(1);
+    });
+
+    test('Refuse check without token', async () => {       
+        // Check Status
+        const response = await supertest(app).post('/communicationrequest/check');
+        expect(response.statusCode).toEqual(500);
+        expect(response.body.error).toBeDefined();        
+    });
+
+    test('Refuse check with invalid token', async () => {       
+        // Check Status
+        const response = await supertest(app).post('/communicationrequest/check').send({
+            "communicationRequestToken": 'ablnmkoqnfoqfnoqqfnqfonqoqwxafdwf'
+        });
+        expect(response.statusCode).toEqual(500);
+        expect(response.body.error).toBeDefined();        
+    });
+}
 executeRegisterCommunicationTests();
+executeCheckCommunicationTest();
