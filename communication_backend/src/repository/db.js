135d2e56f0b52c8eea6cb4a46928db597da0ff96
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-20 23:22:12 -0300

    :heavy_plus_sign: Adicionando driver de conexão com o banco de dados

--- communication_backend/src/repository/db.js
+++ communication_backend/src/repository/db.js
@@ -1,3 +1,8 @@
-const dbClient = function(){};
-console.log(process.env.DATABASE_HOST);
+const config = {
+    host: process.env.DATABASE_HOST,
+    schema: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
+};
+
 module.exports = dbClient;
