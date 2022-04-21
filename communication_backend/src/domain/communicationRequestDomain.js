fca071386c415ac35b499ca5f3abba4bd4fcd29b
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-21 01:48:32 -0300

    :sparkles: feat: Adiciona métodos para validação de todos os parâmetros da requisição

--- communication_backend/src/domain/communicationRequestDomain.js
+++ communication_backend/src/domain/communicationRequestDomain.js
@@ -2,5 +2,6 @@ const CommunicationRequestDomain = function () { };
 
 CommunicationRequestDomain.communicationTypes = { email: 1, sms: 2, push: 3, whatsapp: 4 };
 CommunicationRequestDomain.communicationStatus = { waiting: 1, sent: 2, cancelled: 3 };
+CommunicationRequestDomain.minimumMessageSizeInCharacters = 10;
 
 module.exports = CommunicationRequestDomain;
--- communication_backend/src/services/communicationRequestService.js
+++ communication_backend/src/services/communicationRequestService.js
@@ -1,15 +1,15 @@
 const { v4: uuidv4 } = require('uuid');
 const communicationRequestDal = require('../repository/communicationRequestDal');
-const communicationDomain = require('../domain/communicationRequestDomain');
+const communicationRequestDomain = require('../domain/communicationRequestDomain');
 
 async function registerCommunicationSend(req, res) {
     let communicationRequest = req.body;
-    if (validateBody(communicationRequest)) {
+    if (validateCommunicationRequest(communicationRequest)) {
         const uniqueCode = uuidv4();
         communicationRequest.id = uniqueCode;
         communicationRequest.deliveryTime = new Date(communicationRequest.deliveryTime);
         let registerConfirmation = await communicationRequestDal.registerCommunicationRequest(communicationRequest,
-            communicationDomain.communicationStatus.waiting);
+            communicationRequestDomain.communicationStatus.waiting);
         if (registerConfirmation)
             res.status(200).json({ code: uniqueCode });
         else
@@ -20,26 +20,32 @@ async function registerCommunicationSend(req, res) {
     }
 }
 
-function validateBody(communicationRequest) {
-    if (!communicationRequest.deliveryTime)
-        return false;
-    else {
-        if (!validateDateAttribute(communicationRequest.deliveryTime))
-            return false;
-    }
-    if (!communicationRequest.receiverEmail)
-        return false;
-    if (!communicationRequest.message)
-        return false;
-    if (!communicationRequest.deliveryType)
-        return false;
+function validateCommunicationRequest(communicationRequest) {
+    if (!communicationRequest.deliveryTime || !validateDateAttribute(communicationRequest.deliveryTime)) return false;
+    if (!communicationRequest.receiverEmail || !validateMailString(communicationRequest.receiverEmail)) return false;
+    if (!communicationRequest.message || !validateMessageString(communicationRequest.message)) return false;
+    if (!communicationRequest.deliveryType || !validateDeliveryType(communicationRequest.deliveryType)) return false;
     return true;
 }
 
 function validateDateAttribute(dateAttribute) {
     if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(dateAttribute)) return false;
-    var d = new Date(dateAttribute); 
-    return d.toISOString()===dateAttribute;
+    var d = new Date(dateAttribute);
+    return d.toISOString() === dateAttribute;
+}
+
+function validateMailString(mailString) {
+    let simpleRegex = /\S+@\S+\.\S+/;
+    return simpleRegex.test(mailString);
+}
+
+function validateMessageString(messageString) {
+    return messageString.length > communicationRequestDomain.minimumMessageSizeInCharacters;
+}
+
+function validateDeliveryType(deliveryType) {
+    let validValues = Object.keys(communicationRequestDomain.communicationTypes).map(key => communicationRequestDomain.communicationTypes[key]);  
+    return validValues.indexOf(deliveryType) != -1;
 }
 
 module.exports = { registerCommunicationSend };
